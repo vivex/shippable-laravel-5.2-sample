00444e09c387b11131e5a335fec75c536e109424
vivek soni <vicky.soni@ymail.com>
2017-07-27 13:45:20 +0530

    Update server.php

--- server.php
+++ server.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Vivek Otwell <taylorotwell@gmail.com>
+ * @author   Vivek Soni <taylorotwell@gmail.com>
  */
 
 $uri = urldecode(
